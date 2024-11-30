ad6b3b8080b728025af61630c50ad25bbf91461b
Gaman Sai <gamannallamothu@gmail.com>
2024-11-30 23:21:42 +0530

    Login included

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,9 @@
 import UserModel from '../models/User.js'
 import bcrypt from 'bcrypt'
+import generateTokens from '../utils/generateTokens.js';
+import setTokensCookies from '../utils/setTokensCookies.js';
+
+
 
 class UserController {
   // User Registration
@@ -42,6 +46,56 @@ class UserController {
       res.status(500).json({ status: "failed", message: "Unable to Register, please try again later" });
     }
   };
+  // User Login
+  static userLogin = async (req, res) => {
+    try {
+      const { email, password } = req.body
+      // Check if email and password are provided
+      if (!email || !password) {
+        return res.status(400).json({ status: "failed", message: "Email and password are required" });
+      }
+      // Find user by email
+      const user = await UserModel.findOne({ email });
+
+      // Check if user exists
+      if (!user) {
+        return res.status(404).json({ status: "failed", message: "Invalid Email or Password" });
+      }
+
+      // Check if user verified
+      // if (!user.is_verified) {
+      //   return res.status(401).json({ status: "failed", message: "Your account is not verified" });
+      // }
+
+      // Compare passwords / Check Password
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(401).json({ status: "failed", message: "Invalid email or password" });
+      }
+
+      // Generate tokens
+      const { accessToken, refreshToken, accessTokenExp, refreshTokenExp } = await generateTokens(user)
+
+      // Set Cookies
+      setTokensCookies(res, accessToken, refreshToken, accessTokenExp, refreshTokenExp)
+
+      // Send success response with tokens
+      res.status(200).json({
+        user: { id: user._id, email: user.email, name: user.name, roles: user.roles[0] },
+        status: "success",
+        message: "Login successful",
+        access_token: accessToken,
+        refresh_token: refreshToken,
+        access_token_exp: accessTokenExp,
+        is_auth: true
+      });
+
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ status: "failed", message: "Unable to login, please try again later" });
+    }
+  }
   
 
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,5 +5,7 @@ import UserController from '../controllers/userController.js';
 
 // Public Routes
 router.post('/register', UserController.userRegistration)
+router.post('/login', UserController.userLogin)
 
-export default router 
+
+export default router  
