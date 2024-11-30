8fbe3b8092c0344459edb6adafb5dff86f2de2a1
Gaman Sai <gamannallamothu@gmail.com>
2024-12-01 00:43:41 +0530

    backend push auth

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
- import dotenv from 'dotenv'
+import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 import cors from 'cors';
@@ -6,31 +6,33 @@ import cookieParser from 'cookie-parser';
 import connectDB from './config/connectdb.js'
 import passport from 'passport';
 import userRoutes from './routes/userRoutes.js'
-
+import './config/passport-jwt-strategy.js'
 const app = express()
-
 const port = process.env.PORT
-
-const DATABASE_URL = process.env.DATABASE_URL;
-
+const DATABASE_URL = process.env.DATABASE_URL
+// This will solve CORS Policy Error
 const corsOptions = {
-    // set origin to a specific origin.
-    origin: process.env.FRONTEND_HOST,
-    credentials: true,
-    optionsSuccessStatus: 200,
+  // set origin to a specific origin.
+  origin: process.env.FRONTEND_HOST,
+  credentials: true,
+  optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions))
 
+ 
+
+// JSON
 app.use(express.json())
 
+// Passport Middleware
 app.use(passport.initialize());
 
+// Cookie Parser
 app.use(cookieParser())
 
-connectDB(DATABASE_URL)
-
-app.use("/api/user",userRoutes)
+// Load Routes
+app.use("/api/user", userRoutes)
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`)
-  })
+  console.log(`Server listening at http://localhost:${port}`)
+})
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,13 @@
 import UserModel from '../models/User.js'
 import bcrypt from 'bcrypt'
+// import sendEmailVerificationOTP from '../utils/sendEmailVerificationOTP.js';
+
 import generateTokens from '../utils/generateTokens.js';
 import setTokensCookies from '../utils/setTokensCookies.js';
-
-
-
+import refreshAccessToken from '../utils/refreshAccessToken.js';
+import UserRefreshTokenModel from '../models/UserRefreshToken.js';
+import jwt from "jsonwebtoken"
+import transporter from '../config/emailConfig.js';
 class UserController {
   // User Registration
   static userRegistration = async (req, res) => {
@@ -46,6 +49,65 @@ class UserController {
       res.status(500).json({ status: "failed", message: "Unable to Register, please try again later" });
     }
   };
+
+  // User Email Verification
+  // static verifyEmail = async (req, res) => {
+  //   try {
+
+  //     // Extract request body parameters
+  //     const { email, otp } = req.body;
+
+  //     // Check if all required fields are provided
+  //     if (!email || !otp) {
+  //       return res.status(400).json({ status: "failed", message: "All fields are required" });
+  //     }
+
+  //     const existingUser = await UserModel.findOne({ email });
+
+  //     // Check if email doesn't exists
+  //     if (!existingUser) {
+  //       return res.status(404).json({ status: "failed", message: "Email doesn't exists" });
+  //     }
+
+  //     // Check if email is already verified
+  //     if (existingUser.is_verified) {
+  //       return res.status(400).json({ status: "failed", message: "Email is already verified" });
+  //     }
+
+  //     // Check if there is a matching email verification OTP
+  //     const emailVerification = await EmailVerificationModel.findOne({ userId: existingUser._id, otp });
+  //     if (!emailVerification) {
+  //       if (!existingUser.is_verified) {
+  //         // console.log(existingUser);
+  //         await sendEmailVerificationOTP(req, existingUser);
+  //         return res.status(400).json({ status: "failed", message: "Invalid OTP, new OTP sent to your email" });
+  //       }
+  //       return res.status(400).json({ status: "failed", message: "Invalid OTP" });
+  //     }
+
+  //     // Check if OTP is expired
+  //     const currentTime = new Date();
+  //     // 15 * 60 * 1000 calculates the expiration period in milliseconds(15 minutes).
+  //     const expirationTime = new Date(emailVerification.createdAt.getTime() + 15 * 60 * 1000);
+  //     if (currentTime > expirationTime) {
+  //       // OTP expired, send new OTP
+  //       await sendEmailVerificationOTP(req, existingUser);
+  //       return res.status(400).json({ status: "failed", message: "OTP expired, new OTP sent to your email" });
+  //     }
+
+  //     // OTP is valid and not expired, mark email as verified
+  //     existingUser.is_verified = true;
+  //     await existingUser.save();
+
+  //     // Delete email verification document
+  //     await EmailVerificationModel.deleteMany({ userId: existingUser._id });
+  //     return res.status(200).json({ status: "success", message: "Email verified successfully" });
+  //   } catch (error) {
+  //     console.error(error);
+  //     res.status(500).json({ status: "failed", message: "Unable to verify email, please try again later" });
+  //   }
+  // }
+
   // User Login
   static userLogin = async (req, res) => {
     try {
@@ -97,7 +159,7 @@ class UserController {
     }
   }
 
-  //Get New Access Token OR Refresh Token
+  // Get New Access Token OR Refresh Token
   static getNewAccessToken = async (req, res) => {
     try {
       // Get new access token using Refresh Token
@@ -119,8 +181,141 @@ class UserController {
       res.status(500).json({ status: "failed", message: "Unable to generate new token, please try again later" });
     }
   }
-  
 
+  // Profile OR Logged in User
+  static userProfile = async (req, res) => {
+    res.send({ "user": req.user })
+  }
+
+  // Change Password
+  static changeUserPassword = async (req, res) => {
+    try {
+      const { password, password_confirmation } = req.body;
+
+      // Check if both password and password_confirmation are provided
+      if (!password || !password_confirmation) {
+        return res.status(400).json({ status: "failed", message: "New Password and Confirm New Password are required" });
+      }
+
+      // Check if password and password_confirmation match
+      if (password !== password_confirmation) {
+        return res.status(400).json({ status: "failed", message: "New Password and Confirm New Password don't match" });
+      }
+
+      // Generate salt and hash new password
+      const salt = await bcrypt.genSalt(10);
+      const newHashPassword = await bcrypt.hash(password, salt);
+
+      // Update user's password
+      await UserModel.findByIdAndUpdate(req.user._id, { $set: { password: newHashPassword } });
+
+      // Send success response
+      res.status(200).json({ status: "success", message: "Password changed successfully" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ status: "failed", message: "Unable to change password, please try again later" });
+    }
+  }
+
+  // Send Password Reset Link via Email
+  static sendUserPasswordResetEmail = async (req, res) => {
+    try {
+      const { email } = req.body;
+      // Check if email is provided
+      if (!email) {
+        return res.status(400).json({ status: "failed", message: "Email field is required" });
+      }
+      // Find user by email
+      const user = await UserModel.findOne({ email });
+      if (!user) {
+        return res.status(404).json({ status: "failed", message: "Email doesn't exist" });
+      }
+      // Generate token for password reset
+      const secret = user._id + process.env.JWT_ACCESS_TOKEN_SECRET_KEY;
+      const token = jwt.sign({ userID: user._id }, secret, { expiresIn: '15m' });
+      // Reset Link
+      const resetLink = `${process.env.FRONTEND_HOST}/account/reset-password-confirm/${user._id}/${token}`;
+      console.log(resetLink);
+      // Send password reset email  
+      await transporter.sendMail({
+        from: process.env.EMAIL_FROM,
+        to: user.email,
+        subject: "Password Reset Link",
+        html: `<p>Hello ${user.name},</p><p>Please <a href="${resetLink}">click here</a> to reset your password.</p>`
+      });
+      // Send success response
+      res.status(200).json({ status: "success", message: "Password reset email sent. Please check your email." });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ status: "failed", message: "Unable to send password reset email. Please try again later." });
+    }
+  }
+
+  // Password Reset
+  static userPasswordReset = async (req, res) => {
+    try {
+      const { password, password_confirmation } = req.body;
+      const { id, token } = req.params;
+      // Find user by ID
+      const user = await UserModel.findById(id);
+      if (!user) {
+        return res.status(404).json({ status: "failed", message: "User not found" });
+      }
+      // Validate token
+      const new_secret = user._id + process.env.JWT_ACCESS_TOKEN_SECRET_KEY;
+      jwt.verify(token, new_secret);
+
+      // Check if password and password_confirmation are provided
+      if (!password || !password_confirmation) {
+        return res.status(400).json({ status: "failed", message: "New Password and Confirm New Password are required" });
+      }
+
+      // Check if password and password_confirmation match
+      if (password !== password_confirmation) {
+        return res.status(400).json({ status: "failed", message: "New Password and Confirm New Password don't match" });
+      }
+
+      // Generate salt and hash new password
+      const salt = await bcrypt.genSalt(10);
+      const newHashPassword = await bcrypt.hash(password, salt);
+
+      // Update user's password
+      await UserModel.findByIdAndUpdate(user._id, { $set: { password: newHashPassword } });
+
+      // Send success response
+      res.status(200).json({ status: "success", message: "Password reset successfully" });
+
+    } catch (error) {
+      console.log(error);
+      if (error.name === "TokenExpiredError") {
+        return res.status(400).json({ status: "failed", message: "Token expired. Please request a new password reset link." });
+      }
+      return res.status(500).json({ status: "failed", message: "Unable to reset password. Please try again later." });
+    }
+  }
+
+  // Logout
+  static userLogout = async (req, res) => {
+    try {
+      // Optionally, you can blacklist the refresh token in the database
+      const refreshToken = req.cookies.refreshToken;
+      await UserRefreshTokenModel.findOneAndUpdate(
+        { token: refreshToken },
+        { $set: { blacklisted: true } }
+      );
+
+      // Clear access token and refresh token cookies
+      res.clearCookie('accessToken');
+      res.clearCookie('refreshToken');
+      res.clearCookie('is_auth');
+
+      res.status(200).json({ status: "success", message: "Logout successful" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ status: "failed", message: "Unable to logout, please try again later" });
+    }
+
+  }
 }
 
 export default UserController
